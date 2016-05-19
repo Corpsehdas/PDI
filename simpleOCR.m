d05039f095183ae0f04fe1f8c7be71eaa856d3ad
Andres Lopez <Andres Lopez>
2016-05-19 14:18:35 -0500

    for to while

--- simpleOCR.m
+++ simpleOCR.m
@@ -1,4 +1,6 @@
+
 function simpleOcr()
+    
     close all
     clc
 
@@ -16,27 +18,25 @@ function simpleOcr()
     
     %% predicción de imagen completa   
     % se obtienen los segmentos de la imagen
-    segments = getSegmentsFromImage('images/25.jpg',0);
+    segments = getSegmentsFromImage('images/43.jpg',0);
     numObj = length(segments);
-    
     %show image
-    imageToTest = imread('images/25.jpg');
+    imageToTest = imread('images/43.jpg');
     figure; imshow(imageToTest); hold on;
     title('Imagen Original'); 
     
     %dibujar segmentos y 
-    for k = 1 : numObj - 1      
-        u = size(segments(k).image);
-        if u(1)<10
+    k= 1; 
+    while k ~= numObj + 1
+        if sum(sum(segments(k).image)) < 2500
             segments(k) = [];  
             numObj = numObj  - 1;
-        end    
-        label = predictionGivenImage(model,segments(k).image);
-    end
-    for k = 1 : numObj  
+        end  
         label = predictionGivenImage(model,segments(k).image);
         rectangle('Position', segments(k).bBox,'EdgeColor','r');
-        text(segments(k).bBox(1) + 2*segments(k).center(2),segments(k).bBox(2)+ segments(k).center(1),num2str(label),'Color','b','FontSize',25);
+        text(segments(k).bBox(1) + segments(k).center(2),segments(k).bBox(2)... 
+            + segments(k).center(1),num2str(label),'Color','r','FontSize',15);
+        k = k+1;
     end    
 end
 
