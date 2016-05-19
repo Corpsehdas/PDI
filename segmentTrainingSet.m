3c37f5bf46654a78f39f0faa55d4021d29ddbde8
Andres F. Lopez <ing.ratosocial@gmail.com>
2016-05-19 14:43:21 -0500

    file selection & names

--- segmentTrainingSet.m
+++ segmentTrainingSet.m
@@ -1,36 +1,33 @@
-
-
 function segmentTrainingSet()
 clc
 clear all
 close all
-
-    folderName ='arialSegmented';
-    mkdir (['trainingSet/' folderName '/' ]);
+    carpetaEntrenamiento ='nums';
+    mkdir (['trainingSet/' carpetaEntrenamiento '/' ]);
     %% se segmentan los números
     for i = 1:5 
         disp(['Segmentando training set: ' num2str(i)]);
-        mkdir (['trainingSet/' folderName '/'  num2str(i)]);        
-        getSegmentsFromTrainingSet(i,folderName);
+        mkdir (['trainingSet/' carpetaEntrenamiento '/'  num2str(i)]);        
+        getSegmentsFromTrainingSet(i,carpetaEntrenamiento);
     end
     
     %% se eliminan las imagenes  "ruido"    
     %Se obtiene la lista de clases
-    dirList = dir(['trainingSet/' folderName]);    
+    dirList = dir(['trainingSet/' carpetaEntrenamiento]);    
     for ndir=1:length(dirList)
         if ~(strcmp(dirList(ndir).name, '.') || strcmp(dirList(ndir).name, '..'))
             if dirList(ndir).isdir == 1,
                 disp(['--Limpiando training set: ' dirList(ndir).name]);        
 
                 % se obtiene la lista de imagenes de cada clase
-                imageList = dir(['trainingSet/' folderName '/' dirList(ndir).name '/*.png']);
+                imageList = dir(['trainingSet/' carpetaEntrenamiento '/' dirList(ndir).name '/*.png']);
                 area = [];
                 cont = 1;                
                 % se obtiene la metrica que nos servirá para eliminar las
                 % imágenes diferentes a las esperadas,área
                 for nImage=1:length(imageList)
                     %se obtiene la imagen actual
-                    currentImage = imread(['trainingSet/' folderName '/' dirList(ndir).name '/' imageList(nImage).name]);
+                    currentImage = imread(['trainingSet/' carpetaEntrenamiento '/' dirList(ndir).name '/' imageList(nImage).name]);
                     %se halla area de la letra
                     area(cont,1) = sum(sum(currentImage));
                     cont = cont + 1;                    
@@ -42,15 +39,15 @@ close all
                 %se eliminan imagenes basura
                 for nImage=1:length(imageList)
                     %se obtiene la imagen actual
-                    currentImage = imread(['trainingSet/' folderName '/' dirList(ndir).name '/' imageList(nImage).name]);
+                    currentImage = imread(['trainingSet/' carpetaEntrenamiento '/' dirList(ndir).name '/' imageList(nImage).name]);
                     %se halla area de la letra
                     area_current = sum(sum(currentImage));
                     % se elimina si está afuera de los límites
                     if area_current < threshold_low
-                        delete(['trainingSet/' folderName '/' dirList(ndir).name '/' imageList(nImage).name]);
+                        delete(['trainingSet/' carpetaEntrenamiento '/' dirList(ndir).name '/' imageList(nImage).name]);
                     end
                     if area_current > threshold_high
-                        delete(['trainingSet/' folderName '/' dirList(ndir).name '/' imageList(nImage).name]);
+                        delete(['trainingSet/' carpetaEntrenamiento '/' dirList(ndir).name '/' imageList(nImage).name]);
                     end
                 end
                 
